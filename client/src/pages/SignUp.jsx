4de0b433d3bbf79343c84d406cb49f0026055382
Manoj <manojgowdacs35@gmail.com>
2023-12-26 21:44:25 +0530

    complete signup function

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {Link,useNavigate} from 'react-router-dom'
+import OAuth from '../component/OAuth';
 
 const SignUp = () => {
 
@@ -54,6 +55,7 @@ const SignUp = () => {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80 '>
           {loading ? 'Loading...' : 'Sign up'}
           </button>
+          <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
