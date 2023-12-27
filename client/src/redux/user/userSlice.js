ca27f89bb77c634211e7f2cfb9af315e1297d9b3
Manoj <manojgowdacs35@gmail.com>
2023-12-27 22:42:47 +0530

    delete user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,14 +21,59 @@ const userSlice = createSlice({
         signInFailure : (state,action)=>{
             state.error= action.payload;
             state.loading= false;
+        },
+        updateUserStart : (state) =>{
+            state.loading = true;
+        },
+        updateUserSuccess : (state,action)=>{
+            state.currentUser = action.payload;
+            state.error = null;
+            state.loading= false;
+        },
+        updateUserFailure : (state,action) =>{
+            state.error = action.payload;
+            state.loading= false;
+        },
+        deleteUserStart: (state)=> {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state,action)=>{
+            state.currentUser = null;
+            state.loading = false;
+            state.error = false;
+        },
+        deleteUserFailure : (state,action)=>{
+            state.error= action.payload;
+            state.loading = false;
+        },
+        signOutUserStart: (state)=> {
+            state.loading = true;
+        },
+        signOutUserSuccess: (state,action)=>{
+            state.currentUser = null;
+            state.loading = false;
+            state.error = false;
+        },
+        signOutUserFailure : (state,action)=>{
+            state.error= action.payload;
+            state.loading = false;
         }
     }
-})
+    }) 
 
 export const {
     signInStart,
     signInSuccess,
-    signInFailure
+    signInFailure,
+    updateUserStart,
+    updateUserSuccess,
+    updateUserFailure,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFailure,
+    signOutUserStart,
+    signOutUserSuccess,
+    signOutUserFailure
 } = userSlice.actions;
 
 export default userSlice.reducer;
