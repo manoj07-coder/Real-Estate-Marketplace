d36c903cc48d076dea98fb1bd43a3cb33b41dc22
Manoj <manojgowdacs35@gmail.com>
2023-12-31 12:21:49 +0530

    get listing route created

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express  from 'express';
-import { createListing,deleteListing,updateListing,getListing } from '../controllers/list.controller.js';
+import { createListing,deleteListing,updateListing,getListing, getListings } from '../controllers/list.controller.js';
 import { verifyToken } from '../utils/verify.user.js';
 
 const router =  express.Router();
@@ -8,5 +8,6 @@ router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing)
 router.post('/update/:id',verifyToken,updateListing)
 router.get('/get/:id',getListing)
+router.get('/get',getListings)
 
 export default router;
